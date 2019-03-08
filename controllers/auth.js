2925910a53cf1acd935e4dc5746a1412f43385e4
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 15:59:31 -0500

    able to get sign up form and send signup form

---
+++ controllers/auth.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const User = require("../models/user");
+
+module.exports = function(app) {
+  // SIGN UP FORM
+  app.get("/sign-up", (req, res) => {
+    res.render("sign-up");
+  });
+  app.post("/sign-up", (req, res) => {
+  // Create User
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(user => {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+
+      res.redirect("/");
+    })
+    .catch(err => {
+      console.log(err.message);
+      return res.status(400).send({ err: err });
+
+    });
+});
+app.get('/logout', (req, res) => {
+  res.clearCookie('nToken');
+  res.redirect('/');
+});
+
+
+}
