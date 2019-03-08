077e1262f09c52c50e1767373f2faa873b743b12
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 15:47:13 -0500

    added user schema, and sign in form

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  createdAt: { type: Date },
+  updatedAt: { type: Date },
+  password: { type: String, select: false },
+  username: { type: String, required: true },
+});
