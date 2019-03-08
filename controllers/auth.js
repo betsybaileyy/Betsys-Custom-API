b0c3571f96c1982dea8f4ec1315b70d6742cbf81
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 16:03:46 -0500

    login & logout, cookie stuff

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,12 +3,10 @@ const app = express()
 const User = require("../models/user");
 
 module.exports = function(app) {
-  // SIGN UP FORM
   app.get("/sign-up", (req, res) => {
     res.render("sign-up");
   });
   app.post("/sign-up", (req, res) => {
-  // Create User
   const user = new User(req.body);
 
   user
@@ -30,5 +28,37 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/login', (req, res) => {
+  res.render('sign-in');
+});
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Find this user name
+  User.findOne({ username }, "username password")
+    .then(user => {
+      if (!user) {
+        // User not found
+        return res.status(401).send({ message: "Wrong Username or Password" });
+      }
+      // Check the password
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          // Password does not match
+          return res.status(401).send({ message: "Wrong Username or password" });
+        }
+        // Create a token
+        const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+          expiresIn: "60 days"
+        });
+        // Set a cookie and redirect to root
+        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 }
