06bd67e8701c40f7c2f8cab9f07804c149fc4d12
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 15:57:26 -0500

    whoops forgot compare password and to export

--- models/user.js
+++ models/user.js
@@ -2,9 +2,37 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
+//I re-went through the reddit clone for this code, couldn't remember all the steps on my own :(
+
 const UserSchema = new Schema({
   createdAt: { type: Date },
   updatedAt: { type: Date },
   password: { type: String, select: false },
   username: { type: String, required: true },
 });
+
+UserSchema.pre("save", function(next) {
+  const now = new Date();
+  this.updatedAt = now;
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  const user = this;
+if (!user.isModified("password")) {
+  return next();
+}
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(user.password, salt, (err, hash) => {
+    user.password = hash;
+  next();
+});
+});
+});
+
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
