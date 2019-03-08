beb4e01aae30d1f905064eb38ea0e9102b7546b4
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 11:06:26 -0500

    deleted commented code, onward upward

--- app.js
+++ app.js
@@ -26,68 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(expressValidator())
 
-require('./controllers/posts.js')(app)
-require('./controllers/comments.js')(app)
-require('./controllers/auth.js')(app)
+require('./controllers/favoriteThings.js')(app)
+
+// require('./controllers/auth.js')(app)
 
 app.listen(3000, () => {
     console.log('this thing is on!! port 3000')
 })
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express')
-// const app = express()
-// var exphbs = require('express-handlebars');
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/betsys-custom-api', { useNewUrlParser: true });
-//
-// // const Review = mongoose.model('Review', {
-// //   title: String,
-// //   movieTitle: String
-// // });
-//
-//
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-//
-// app.get('/', (req, res) => {
-//   res.render('views/home', { msg: 'Handlebars are Cool!' });
-// })
-//
-// app.get('/', (req, res) => {
-//   Review.find()
-//     .then(reviews => {
-//       res.render('reviews-index', { reviews: reviews });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     })
-// })
-//
-// app.listen(3000, () => {
-//   console.log('App listening on port 3000!')
-// })
