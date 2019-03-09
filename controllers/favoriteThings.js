75244451c6cd1cd9a278b9023e6ee98b6340c302
betsybaileyy <betsy.bailey80@gmail.com>
2019-03-08 22:01:09 -0500

    users can save post if logged in

--- controllers/favoriteThings.js
+++ controllers/favoriteThings.js
@@ -1,4 +1,5 @@
 const FavoriteThing = require('../models/favoriteThing')
+const User = require('../models/user')
 
 module.exports = (app) => {
     // index of all favorite things
@@ -8,7 +9,8 @@ module.exports = (app) => {
         FavoriteThing.find({})
             .then(favoriteThing => {
                 res.render("index", {
-                    favoriteThing: favoriteThing
+                    favoriteThing,
+                    currentUser
                 })
             })
             .catch(err => {
@@ -17,31 +19,36 @@ module.exports = (app) => {
     })
     //getting form for new fav thing
     app.get('/favoriteThing/new', (req, res) => {
+        // var currentUser = req.user;
         res.render('favoriteThing-new', {})
     })
 
     // create a new fav thing
-    app.post('/favoriteThing/new', (req, res) => {
-        console.log(req.body)
-        const favoriteThing = new FavoriteThing({ ...req.body
-        });
+    app.post("/favoriteThing/new", (req, res) => {
+        if (req.user) {
+            var favoriteThing = new FavoriteThing(req.body);
 
-        // SAVE INSTANCE OF favoriteThing MODEL TO DB
-        favoriteThing.save((err, favoriteThing) => {
-            return res.redirect(`/favoriteThing/:id`);
-        })
-    })
+            favoriteThing.save(function(err, favoriteThing) {
+                return res.redirect(`/`);
+            });
+        } else {
+            return res.status(401); // UNAUTHORIZED
+        }
+    });
     // get fav thing individually
     app.get("/favoriteThing/:id", function(req, res) {
+        var currentUser = req.user;
+
         // LOOK UP THE favoriteThing
         FavoriteThing.findById(req.params.id)
             .then(favoriteThing => {
                 res.render("favoriteThing-show", {
-                    favoriteThing
-                })
-                .catch(err => {
-                console.log(err.message)
-            })
+                        favoriteThing,
+                        currentUser
+                    })
+                    .catch(err => {
+                        console.log(err.message)
+                    })
             })
             .catch(err => {
                 console.log(err.message);
@@ -49,8 +56,13 @@ module.exports = (app) => {
     })
 
     app.get('/favoriteThing/:id/edit', (req, res) => {
+        var currentUser = req.user;
+
         FavoriteThing.findById(req.params.id, function(err, favoriteThing) {
-            res.render('favoriteThing-edit', {favoriteThing: favoriteThing});
+            res.render('favoriteThing-edit', {
+                favoriteThing: favoriteThing,
+                currentUser
+            });
         })
     })
 
